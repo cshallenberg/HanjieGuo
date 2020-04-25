6ca168559438b4c4501726b81d016df94a55565c
cshallenberg <cshallenberg@gmail.com>
2020-04-25 16:15:24 -0500

    Update imageMove.js

--- imageMove.js
+++ imageMove.js
@@ -4,13 +4,13 @@ let img3;
 let img4;
 
 let x1 = 200;
-let y1 = 500;
+let y1;
 let x2 = 500;
-let y2 = 500;
+let y2;
 let x3 = 800;
-let y3 = 500;
+let y3;
 let x4 = 1100;
-let y4 = 500;
+let y4;
 
 function preload() {
   img1 = loadImage('assets/1pad.png');
@@ -22,6 +22,14 @@ function preload() {
 function setup() {
   createCanvas(windowWidth, windowHeight);
   imageMode(CENTER);
+  x1 = width/4;
+  x2 = 2(width/4);
+  x3 = 3(width/4);
+  x4 = 4(width/4);
+  y1 = height/2;
+  y2 = height/2;
+  y3 = height/2;
+  y4 = height/2;    
 }
 
 function windowResized() {
@@ -29,24 +37,21 @@ function windowResized() {
   background(255);
 }
 
-function keyIsDown() {
-  if (key === '1') {
+function draw() {
+  background(255);
+    if (keyIsDown('1')) {
     x1 = mouseX;
     y1 = mouseY;
-  } else if (key === '2') {
+  } else if (keyIsDown('2')) {
     x2 = mouseX;
     y2 = mouseY;
-  } else if (key === '3') {
+  } else if (keyIsDown('3')) {
     x3 = mouseX;
     y3 = mouseY;
-  } else if (key === '4') {
+  } else if (keyIsDown('4')) {
     x4 = mouseX;
     y4 = mouseY;
   }
-}
-
-function draw() {
-  background(255);
   image(img3, x3, y3, img3.width/4, img3.height/4);
   image(img4, x4, y4, img4.width/4, img4.height/4);
   image(img1, x1, y1, img1.width/4, img1.height/4);
@@ -55,5 +60,5 @@ function draw() {
    fill(0);
    textSize(12);
    textAlign(LEFT, TOP);
-   text("hold down numbers 1 though 4 to move the images with your mouse", 10, 10);
+   text("hold down numbers 1 though 4 to move the mind maps with your mouse", 10, 10);
 }
